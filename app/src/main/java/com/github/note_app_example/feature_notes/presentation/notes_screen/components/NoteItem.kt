9f69924a98b5b0a4c913740a3f5ea8cfe0d0e246
lifestreamy <lifestreamy@gmail.com>
2023-06-24 13:08:53 +0300

    Added basic visual components to notes_screen/components, Added copy() to NoteOrder class to be able to preserve NoteOrder while changing OrderType (Ascending/Descending)

---
+++ app/src/main/java/com/github/note_app_example/feature_notes/presentation/notes_screen/components/NoteItem.kt
@@ -0,0 +1,19 @@
+package com.github.note_app_example.feature_notes.presentation.notes_screen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.github.note_app_example.feature_notes.domain.model.Note
+
+@Composable
+fun NoteItem(
+    note: Note,
+    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 10.dp,
+    cutCornerSize: Dp = 30.dp,
+    onDeleteClick: () -> Unit
+) {
+
+}
