cb24f8a735a3a28c787fca69ae061a1429237ed9
lifestreamy <lifestreamy@gmail.com>
2023-07-02 19:04:33 +0300

    Bumped up dependencies. Switched to another color scheme for a Note.
    Bumped up dependencies, cleaned both build.gradle files.

--- app/src/main/java/com/github/note_app_example/feature_notes/domain/model/Note.kt
+++ app/src/main/java/com/github/note_app_example/feature_notes/domain/model/Note.kt
@@ -2,19 +2,24 @@ package com.github.note_app_example.feature_notes.domain.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.github.note_app_example.ui.theme.*
+import com.github.note_app_example.ui.theme.DarkPastelGreen
+import com.github.note_app_example.ui.theme.GoldMetallic
+import com.github.note_app_example.ui.theme.ImperialRed
+import com.github.note_app_example.ui.theme.LightOrange
+import com.github.note_app_example.ui.theme.Verdigris
 
 @Entity
 data class Note(
-        val title: String,
-        val content : String,
-        val timestamp: Long,
-        val color: Int,
-        @PrimaryKey val id: Int? = null
-){
-    companion object{
-        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        //        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+        val noteColors = listOf(ImperialRed, Verdigris, GoldMetallic, DarkPastelGreen, LightOrange)
     }
 }
 
-class InvalidNoteException(message : String) : Exception(message)
+class InvalidNoteException(message: String) : Exception(message)
--- app/src/main/java/com/github/note_app_example/ui/theme/Color.kt
+++ app/src/main/java/com/github/note_app_example/ui/theme/Color.kt
@@ -5,8 +5,23 @@ import androidx.compose.ui.graphics.Color
 val DarkGray = Color(0xFF202020)
 val LightBlue = Color(0xFFD7E8DE)
 
-val RedOrange = Color(0xffffab91)
-val RedPink = Color(0xfff48fb1)
-val BabyBlue = Color(0xff81deea)
-val Violet = Color(0xffcf94da)
-val LightGreen = Color(0xffe7ed9b)
+object NoteColors1 {
+    val RedOrange = Color(0xffffab91)
+    val RedPink = Color(0xfff48fb1)
+    val BabyBlue = Color(0xff81deea)
+    val Violet = Color(0xffcf94da)
+    val LightGreen = Color(0xffe7ed9b)
+}
+
+//Chose these using coolors.co
+//object NoteColors2 {
+    val ImperialRed = Color(0xffF21E36)
+    val Verdigris = Color(0xff7EBDC2)
+    val GoldMetallic = Color(0xffD1AC00)
+    val DarkPastelGreen = Color(0xff0CCA4A)
+    val LightOrange = Color(0xffF9DBBD)
+//}
+
+
+
+
